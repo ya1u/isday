b06bd4ba7577dbfedcccc48a30bbbbb046bfd804
ya1u <yalu001@naver.com>
2023-08-28 18:35:37 +0900

    Add Snap & Helmet Debug

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
-);
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(container);
+
+// root.render(
+//   <HelmetProvider>
+//     <App />
+//   </HelmetProvider>
+// );
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
+} else {
+  root.render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
